8707d0fc35a5ac0d83913ed3e4e8cb72e3647a03
ariane-sarni <arianesarni@gmail.com>
2026-02-05 23:06:46 -0800

    Still messing with conditional variables. Going to clean all of it, then implement in producer.

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ struct BufferArray{
 	private:
 		std::vector<int> buffer;
 		int capacity;
-		int indx = 0;
-		int findx = 0;
+		int back = 0;
+		int front = 0;
 		int count = 0;
 		std::mutex mu;
 		std::condition_variable not_empty;
 		std::condition_variable not_full;
-		
+
 	public:
 	BufferArray(int cap) : capacity(cap), buffer(cap) { };
 
@@ -32,36 +32,47 @@ struct BufferArray{
 			// Alternativley, can do with conditional variables and normal mutex. As follows:
 			std::unique_lock<std::mutex> lock(mu);
 			// If the belt is full, stop spinning. Sleep here.
+			// Return count < capacity.
+			// So if there is space, (for example, cap = 10, count = 5, 5 < 10 = true, so we are not full)
+			// Then we do not spin - we can just work.
+			// So telling other threads: Hey, we are good to go. This is no longer full, feel free to add.
 			not_full.wait(lock, [this]() { return count < capacity;});
-			
-			buffer[indx] = x;
-			findx = (findx + 1) % capacity;
+			buffer[front] = x;
+			front = (front + 1) % capacity;
 			count++;
-			// Must not_empty.notify_one(), since there is now space. 
+			// Must not_empty.notify_one(), since there is now space.
+			// Notifying a SINGLE thread that this is no longer empty.
+			// This blocks normally, because in remove/read, we do not want them to read an empty queue.
+			not_empty.notify_one();
+		}
+
+		int remove() {
+			// Still locking as a global lock.
+			std::unique_lock<std::mutex> lock(mu);
+			not_empty.wait(lock, [this]() {return count > 0;});
+			int val = buffer[back];
+			back = (back + 1) % capacity;
+			count--;
+			not_full.notify_one();
+			return back;
 		}
 // Remove would do following:
-// Wait if belt is empty. 
+// Wait if belt is empty.
 // not_empty wait, count > 0
-// Tell a waiting producer (adder) that there is now space. 
+// Tell a waiting producer (adder) that there is now space.
 // Return disappeared value
-		
-		void printBuffer() {
-			std::shared_lock<std::mutex> lock(mu);
-			for (int i = 0; i < capacity; i++) {
-				std::cout << buffer[i] << std::endl;
-			}
-		}
+
 
 };
 
 
 int main(){
 	// Basically a mutex using RAII principles - dies as soon as its out of scope
-	//std::mutex mu;	
+	//std::mutex mu;
 
-	// std::shared_lock<std::shared_mutex> lock(mu); 
-	// That is a lock for reading! 
-	// 
+	// std::shared_lock<std::shared_mutex> lock(mu);
+	// That is a lock for reading!
+	//
 	// std::unique_lock<std::shared_mutex> lock(mu);
 	// This is a lock for writing!
 
@@ -72,7 +83,7 @@ int main(){
 	// Makes a unique mutex, locks it. This would spin forever.
 
 	BufferArray cool(4);
-	
+
 	//std::thread t1(&BufferArray::add, &cool, 1);
 	std::thread t1 ([&cool]() {
 		cool.add(1);
